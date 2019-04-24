ff2901a33ee787188048da0cb5bc82cc6f6e3a2e
sebas avalos <eduardoseadavalos@gmail.com>
2019-04-24 22:32:11 +0200

    ejercicio de practica

--- persona.py
+++ persona.py
@@ -1,5 +1,12 @@
-# esto es una prueba
-print("que tal mundo?")
-print("chau mundooo")
-print("algo") 
-print("algo2")
+class Persona:
+    nombre_persona= None
+    edad_persona = None
+    def __init__(self,un_nombre,una_edad):
+        self.nombre_persona=un_nombre
+        self.edad_persona=una_edad
+        print("hola soy una persona y me llamo",self.nombre_persona,"y mi edad es",self.edad_persona)
+    def get_nombre_persona(self):
+        return self.nombre_persona
+    def get_edad_persona(self):
+        return self.edad_persona
+yo = Persona("pepito",25)
